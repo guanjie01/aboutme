fa6b6c8acb457bdbca2d58f3e058d1ccf300e4ee
O-GuanjieWang-O <1341987719@qq.com>
2022-11-27 10:43:53 +0800

    update

---
+++ js/moduleVisbilityControl.js
@@ -0,0 +1,30 @@
+var modules = [
+  "hd",
+  "main",
+  "about-me",
+  "work-experience",
+  "project",
+  "Awards",
+  "reference",
+];
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  for (var i = 0; i < modules.length; i++) {
+    document.getElementById(modules[i]).style.display = "none";
+  }
+});
+
+window.onload = async function loadPage() {
+  document.getElementById("app").style.display = "block";
+  await sleep(1200);
+  document.getElementById("app").style.display = "none";
+  document.getElementById(modules[0]).style.display = "block";
+  for (var i = 1; i < modules.length; i++) {
+    document.getElementById(modules[i]).style.display = "block";
+    await sleep(1000);
+  }
+};
