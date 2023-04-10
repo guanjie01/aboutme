ee574ebb940cb6a6308d7ebbd4ed7bb6bc4e9176
O-GuanjieWang-O <1341987719@qq.com>
2023-04-11 00:00:34 +0800

    update

---
+++ js/blogs.js
@@ -0,0 +1,22 @@
+var modules = ["hd", "main", "nav", "Blogs", "reference"];
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  for (var i = 0; i < modules.length; i++) {
+    document.getElementById(modules[i]).style.display = "none";
+  }
+});
+
+window.onload = async function loadPage() {
+  document.getElementById("app").style.display = "block";
+  await sleep(1200);
+  document.getElementById("app").style.display = "none";
+  document.getElementById(modules[0]).style.display = "block";
+  for (var i = 1; i < modules.length; i++) {
+    document.getElementById(modules[i]).style.display = "block";
+    await sleep(1000);
+  }
+};
